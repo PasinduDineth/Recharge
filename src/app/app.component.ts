5da84537aac5b01ab44f2732726b175efba644ae
kavindu peiris <lasandupeiris@gmail.com>
2017-04-18 23:35:40 +0530

    2nd commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
-
+import { Sim } from 'ionic-native';
 import { HomePage } from '../pages/home/home';
 
 
@@ -10,11 +10,28 @@ import { HomePage } from '../pages/home/home';
 })
 export class MyApp {
   rootPage = HomePage;
+  simname: string;
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+
+      Sim.getSimInfo().then((data) => {
+          console.log("data set=>",data),
+          console.log("data set2=>",data.cards)
+      });
+     
+    Sim.hasReadPermission().then(
+  (info) => console.log('Has permission: ', info)
+    );
+
+    Sim.requestReadPermission().then(
+    () => console.log('Permission granted'),
+    () => console.log('Permission denied')
+    );
+
+
       Splashscreen.hide();
       StatusBar.styleDefault();
       
